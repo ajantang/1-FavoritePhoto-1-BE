a0134a2c56a4014300b82921ca3a837fa0fcfbdf
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-10-15 13:44:17 +0900

    feat : change custom error message object name

--- src/constants/error.js
+++ src/constants/error.js
@@ -1,7 +1,7 @@
 // 사용 여부 보류
 // errorHandler에서 err 정보를 추가한 error message 처리 중
 
-export const CUSTOM_ERRORS = {
+export const CUSTOM_ERRORS_CODE = {
   401: "Unauthorized",
   409: "Unique constraint failed on the field",
   404: "Not Found",
