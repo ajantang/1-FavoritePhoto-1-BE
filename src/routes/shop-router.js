a8b28cd3e26f91c3df1d18348f7fa5ba88b2862f
songyoungsub <1502qwem@gmail.com>
2024-10-16 11:38:55 +0900

    feat: 유효성 검사 미들웨어의 export 방식의 변경으로 인한 코드 수정

--- src/routes/shop-router.js
+++ src/routes/shop-router.js
@@ -1,9 +1,9 @@
 import e from "express";
 import shopController from "../controllers/shop-controller.js";
-import validateData from "../middlewares/validateData.js";
+import { validateCreateShopData } from "../middlewares/validateData.js";
 
 const shopRouter = e.Router();
 
-shopRouter.route("/").post(validateData.createShop, shopController.createShop); // 인증 후 req.body에 userId 추가
+shopRouter.route("/").post(validateCreateShopData, shopController.createShop); // 인증 후 req.body에 userId 추가
 
 export default shopRouter;
