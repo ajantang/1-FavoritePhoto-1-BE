decddd8f96391cc32842f3bcee9340336bce7f7a
root <root@DESKTOP-UE7169K>
2024-10-16 13:12:14 +0900

    feat: shop 등록 API에 카드 보유량 수정 코드 추가 shop에 등록 시 해당 유저의 해당 카드의 보유량을 수정하는 코드 추가

--- src/controllers/shop-controller.js
+++ src/controllers/shop-controller.js
@@ -1,10 +1,11 @@
+import ownService from "../services/ownService.js";
 import shopService from "../services/shopService.js";
 import { createShopMapper } from "./mappers/shopMapper.js";
 
 async function createShop(req, res, next) {
   const shop = await shopService.createShop(req.body);
+  await ownService.update(req.body);
   const responseData = createShopMapper(shop);
-  // 해당 유저의 해당 카드 보유량 수정 코드 작성
   res.status(201).send(responseData);
 }
 
