a7e44148542e91b933b468843dbf7f432c772bee
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-10-16 16:31:32 +0900

    feat : change struct code
    기존에 사용하던 버전과 사용 방법이 변경되어 수정

--- src/structs/user-struct.js
+++ src/structs/user-struct.js
@@ -1,24 +1,16 @@
-import * as ss from "superstruct";
+import { object, refine, size, string } from "superstruct";
 
 import user from "../constants/user";
 import { emailPattern } from "./patterns/pattern";
 
-export const User = ss.object({
-  email: ss.refine(
+export const User = object({
+  email: refine(
     emailPattern,
     "Email Length",
     (value) =>
       user.EMAIL_MIN_LENGTH <= value.length &&
       value.length <= user.EMAIL_MAX_LENGTH
   ),
-  nickname: ss.size(
-    ss.string(),
-    user.NICKNAME_MIN_LENGTH,
-    user.NICKNAME_MAX_LENGTH
-  ),
-  password: ss.size(
-    ss.string(),
-    user.PASSWORD_MIN_LENGTH,
-    user.PASSWORD_MAX_LENGTH
-  ),
+  nickname: size(string(), user.NICKNAME_MIN_LENGTH, user.NICKNAME_MAX_LENGTH),
+  password: size(string(), user.PASSWORD_MIN_LENGTH, user.PASSWORD_MAX_LENGTH),
 });
