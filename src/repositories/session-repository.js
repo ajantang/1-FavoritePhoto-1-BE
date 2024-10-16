199f6668f611b0abe4c9646a69d46fbfba8cf7d0
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-10-16 13:07:58 +0900

    fix : missing properties & typing error of schema
    오타 수정 및 속성 누락 추가
    DB table sesseion 오타 수정

--- src/repositories/session-repository.js
+++ src/repositories/session-repository.js
@@ -4,21 +4,21 @@ import { sessionSelect } from "./selects/session-select.js";
 async function createSession({ sessionId, userId, expires, data }) {
   const jsonData = JSON.stringify(data);
 
-  return prisma.session.create({
+  return await prisma.session.create({
     data: { id: sessionId, userId, expires, data: jsonData },
     select: sessionSelect,
   });
 }
 
 async function findSession(sessionId) {
-  return prisma.session.findUniqueOrThrow({
+  return await prisma.session.findUniqueOrThrow({
     where: { id: sessionId },
     select: sessionSelect,
   });
 }
 
 async function deleteSession(sessionId) {
-  return prisma.session.delete({ where: { sessionId } });
+  return await prisma.session.delete({ where: { id: sessionId } });
 }
 
 export default { createSession, findSession, deleteSession };
