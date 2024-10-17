c0359823698a84fc06f49d01fd357a94a1615abc
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-10-17 14:50:30 +0900

    feat : add user API /GET /users/my-cards/shop & change shop API
    내 상점 목록 조회 API 추가
    상점 등록 API 로직 수정(논리적인 로직 순서와 불필요한 인증 로직 삭제)

--- src/repositories/shopRepository.js
+++ src/repositories/shopRepository.js
@@ -43,10 +43,57 @@ async function checkUserShopOwner(filter) {
   });
 }
 
+async function findMyShopList({ userId, filter }) {
+  const { orderBy, skip, take, where } = filter;
+
+  return await prisma.shop.findMany({
+    orderBy,
+    skip,
+    take,
+    where: { userId, ...where },
+    select: shopListSelect,
+  });
+}
+
+async function getGroupCountByGrade({ userId, filter }) {
+  const { where } = filter;
+
+  const shops = await prisma.shop.findMany({
+    where: {
+      userId,
+      ...where,
+    },
+    select: {
+      remainingQuantity: true,
+      Card: {
+        select: {
+          grade: true,
+        },
+      },
+    },
+  });
+
+  const counts = shops.reduce((acc, shop) => {
+    const grade = shop.Card.grade;
+
+    if (!acc[grade]) {
+      acc[grade] = 0;
+    }
+
+    acc[grade] += shop.remainingQuantity;
+
+    return acc;
+  }, {});
+
+  return counts;
+}
+
 export default {
   createShop,
   getShopListByQuery,
   countShopListByQuery,
   getShopDetailById,
   checkUserShopOwner,
+  findMyShopList,
+  getGroupCountByGrade,
 };
--- src/routes/shop-router.js
+++ src/routes/shop-router.js
@@ -1,12 +1,13 @@
 import express from "express";
 import shopController from "../controllers/shop-controller.js";
 import { validateCreateShopData } from "../middlewares/validateData.js";
+import { authMiddleware } from "../middlewares/auth.js";
 
 const shopRouter = express.Router();
 
 shopRouter
   .route("/")
-  .post(validateCreateShopData, shopController.createShop) // 인증 후 req.body에 userId 추가
+  .post(authMiddleware, validateCreateShopData, shopController.createShop) // 인증 후 req.body에 userId 추가
   .get(shopController.getShopList);
 
 shopRouter.route("/:id").get(shopController.getShopDetail);
