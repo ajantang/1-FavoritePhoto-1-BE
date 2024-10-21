baa71c04d88effc1855cd2ecacb15950a69d3522
root <root@DESKTOP-UE7169K>
2024-10-21 15:41:57 +0900

    feat: PR 리뷰 확인 후 코드 수정

--- src/services/shopService.js
+++ src/services/shopService.js
@@ -5,7 +5,10 @@ import prisma from "../repositories/prisma.js";
 import purchaseRepository from "../repositories/purchase-repository.js";
 import shopRepository from "../repositories/shopRepository.js";
 import userRepository from "../repositories/user-repository.js";
-import { myCardMapper } from "../controllers/mappers/card-mapper.js";
+import {
+  basicCardMapper,
+  myCardMapper,
+} from "../controllers/mappers/card-mapper.js";
 import { ownCardListSelect } from "../repositories/selects/own-select.js";
 
 async function createShop(createData) {
@@ -221,7 +224,24 @@ async function purchaseService(id, userId, purchaseData) {
         const exchangeDelete = await Promise.all(
           exchangesCardInfo.map(async (exchangeInfo) => {
             const id = exchangeInfo.id;
+            const userId = exchangeInfo.userId;
+            const cardId = exchangeInfo.Card.id;
+            const updateWhere = {
+              userId_cardId: {
+                userId,
+                cardId,
+              },
+            };
+            const updateData = {
+              quantity: {
+                increment: 1,
+              },
+            };
             await exchangeRepository.deleteByExchangeId(id);
+            await ownRepository.updateData({
+              where: { updateWhere },
+              data: { updateData },
+            });
           })
         );
       }
@@ -260,9 +280,10 @@ async function purchaseService(id, userId, purchaseData) {
         });
       }
 
+      const responseMapping = basicCardMapper(purchaserOwn.Card);
+
       return {
-        grade: purchaserOwn.Card.grade,
-        name: purchaserOwn.Card.name,
+        ...responseMapping,
         purchaseQuantity,
       };
     } catch (e) {
