c653fe33b5aa4796ea83f306850cdf9732b482d6
root <root@DESKTOP-UE7169K>
2024-10-21 14:37:52 +0900

    feat: 추후 사용할 repository 코드 추가

--- src/repositories/shopRepository.js
+++ src/repositories/shopRepository.js
@@ -109,6 +109,44 @@ async function findShopOwnerId(id) {
   });
 }
 
+async function createData({ data, select }) {
+  return await prisma.shop.create({ data, select });
+}
+
+async function findFirstData({ where, select }) {
+  return await prisma.shop.findFirst({ where, select });
+}
+
+async function findUniqueOrThrowtData({ where, select }) {
+  return await prisma.shop.findUniqueOrThrow({ where, select });
+}
+
+async function conutData(where) {
+  return await prisma.shop.count({ where });
+}
+
+async function findManyData({ where, select }) {
+  return await prisma.shop.findMany({ where, select });
+}
+
+async function findManyByPaginationData({
+  orderBy,
+  skip,
+  take,
+  where,
+  select,
+}) {
+  return await prisma.shop.findMany({ orderBy, skip, take, where, select });
+}
+
+async function updateData({ where, data, select }) {
+  return await prisma.shop.update({ where, data, select });
+}
+
+async function deleteData(where) {
+  await prisma.shop.delete({ where });
+}
+
 export default {
   createShop,
   getShopListByQuery,
@@ -120,4 +158,12 @@ export default {
   getGroupCountByGrade,
   deleteShop,
   findShopOwnerId,
+  createData,
+  findFirstData,
+  findUniqueOrThrowtData,
+  conutData,
+  findManyData,
+  findManyByPaginationData,
+  updateData,
+  deleteData,
 };
