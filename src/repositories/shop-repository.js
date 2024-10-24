44b60c24a2b5e0fa5ffd22bc664e9d1f2b4b5cb8
root <root@DESKTOP-UE7169K>
2024-10-24 14:05:06 +0900

    refactor: 사용하지 않는 shop-repository 함수 삭제

--- src/repositories/shop-repository.js
+++ src/repositories/shop-repository.js
@@ -1,81 +1,5 @@
 import prisma from "./prisma.js";
 
-import {
-  shopCreateSelect,
-  shopDetailSelect,
-  shopListSelect,
-  shopOwnerSelect,
-} from "../services/selects/shop-select.js";
-
-async function createShop(createData) {
-  return await prisma.shop.create({
-    data: createData,
-    select: shopCreateSelect,
-  });
-}
-
-async function getShopListByQuery(filter) {
-  const { orderBy, skip, take, where } = filter;
-
-  return prisma.shop.findMany({
-    orderBy,
-    skip,
-    take,
-    where,
-    select: shopListSelect,
-  });
-}
-
-async function countShopListByQuery(filter) {
-  return prisma.shop.count({
-    where: filter,
-  });
-}
-
-async function getShopDetailById(id) {
-  return prisma.shop.findUniqueOrThrow({
-    where: { id },
-    select: shopDetailSelect,
-  });
-}
-
-async function checkUserShopOwner(filter) {
-  return prisma.shop.findFirst({
-    where: filter,
-  });
-}
-
-async function updateShop(id, updateData) {
-  return prisma.shop.update({
-    where: { id },
-    data: updateData,
-    select: shopCreateSelect,
-  });
-}
-
-async function findMyShopList({ userId, filter }) {
-  const { orderBy, skip, take, where } = filter;
-
-  return await prisma.shop.findMany({
-    orderBy,
-    skip,
-    take,
-    where: { userId, ...where },
-    select: shopListSelect,
-  });
-}
-
-async function deleteShop(id) {
-  await prisma.shop.delete({ where: { id } });
-}
-
-async function findShopOwnerId(id) {
-  return await prisma.shop.findFirst({
-    where: { id },
-    select: shopOwnerSelect,
-  });
-}
-
 async function createData({ data, select }) {
   return await prisma.shop.create({ data, select });
 }
@@ -115,15 +39,6 @@ async function deleteData(where) {
 }
 
 export default {
-  createShop,
-  getShopListByQuery,
-  countShopListByQuery,
-  getShopDetailById,
-  checkUserShopOwner,
-  updateShop,
-  findMyShopList,
-  deleteShop,
-  findShopOwnerId,
   createData,
   findFirstData,
   findUniqueOrThrowtData,
