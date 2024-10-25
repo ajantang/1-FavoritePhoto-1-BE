42cb7efc5a2db482eaa02251097bcb465ae5589d
root <root@DESKTOP-UE7169K>
2024-10-25 16:43:00 +0900

    feat: controller와 service에 try..catch문 적용

--- src/services/exchange-service.js
+++ src/services/exchange-service.js
@@ -30,52 +30,59 @@ async function checkExchangeByUser(userId, shopId) {
 
 async function createExchange({ userId, shopId, cardId, description }) {
   return prisma.$transaction(async () => {
-    const updateWhere = {
-      userId_cardId: {
+    try {
+      const updateWhere = {
+        userId_cardId: {
+          userId,
+          cardId,
+        },
+      };
+      const updateData = {
+        quantity: { decrement: EXCHANGE_VOLUME },
+      };
+      const update = await ownRepository.updateData({
+        where: updateWhere,
+        data: updateData,
+        select: ownCardSelect,
+      });
+
+      if (update.quantity === 0) {
+        const deleteWhere = { id: update.id };
+        await ownRepository.deleteData(deleteWhere);
+      }
+
+      const shpoWhere = { id: shopId };
+      const updateShopData = { hasExchangeRequest: true };
+      await shopRepository.updateData({
+        where: shpoWhere,
+        data: updateShopData,
+      });
+
+      const exchangeData = { userId, shopId, cardId, description };
+
+      const exchange = await exchangeRepository.createData({
+        data: exchangeData,
+        select: exchangeCardShopSelect,
+      });
+
+      const { message, sellerId } = await createNotificationMessage({
+        idx: EXCHANGE_PROPOSAL_INDEX,
         userId,
-        cardId,
-      },
-    };
-    const updateData = {
-      quantity: { decrement: EXCHANGE_VOLUME },
-    };
-    const update = await ownRepository.updateData({
-      where: updateWhere,
-      data: updateData,
-      select: ownCardSelect,
-    });
-
-    if (update.quantity === 0) {
-      const deleteWhere = { id: update.id };
-      await ownRepository.deleteData(deleteWhere);
-    }
-
-    const shpoWhere = { id: shopId };
-    const updateShopData = { hasExchangeRequest: true };
-    await shopRepository.updateData({ where: shpoWhere, data: updateShopData });
-
-    const exchangeData = { userId, shopId, cardId, description };
-
-    const exchange = await exchangeRepository.createData({
-      data: exchangeData,
-      select: exchangeCardShopSelect,
-    });
-
-    const { message, sellerId } = await createNotificationMessage({
-      idx: EXCHANGE_PROPOSAL_INDEX,
-      userId,
-      shopId,
-    });
-
-    const notification = await notificationRepository.createData({
-      data: {
-        userId: sellerId,
         shopId,
-        message,
-      },
-    });
+      });
 
-    return exchangeCreateMapper(exchange);
+      const notification = await notificationRepository.createData({
+        data: {
+          userId: sellerId,
+          shopId,
+          message,
+        },
+      });
+
+      return exchangeCreateMapper(exchange);
+    } catch (e) {
+      throw e;
+    }
   });
 }
 
