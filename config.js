1f9835ed63a259349ea5b43abf6664bc9e8706c9
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-10-28 16:36:12 +0900

    feat : change set env file logic
    .env 파일을 읽어오는 문제 확인. override로 파일 정보 강제 갱신 설정

--- config.js
+++ config.js
@@ -6,3 +6,6 @@ export const SESSION_SECRET = config.SESSION_SECRET;
 export const SESSION_SECURE = config.SESSION_SECURE;
 export const SESSION_SAMESITE = config.SESSION_SAMESITE;
 export const REDIS_URL = config.REDIS_URL;
+export const AWS_S3_REGION = config.AWS_S3_REGION;
+export const AWS_ACCESS_KEY_ID = config.AWS_ACCESS_KEY_ID;
+export const AWS_SECRET_ACCESS_KEY = config.AWS_SECRET_ACCESS_KEY;
