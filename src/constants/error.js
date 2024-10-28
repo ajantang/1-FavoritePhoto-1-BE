7bd9b3dd98e002ff1049edaf575a2309cbb85ad4
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-10-28 17:32:14 +0900

    feat : add custom error message

--- src/constants/error.js
+++ src/constants/error.js
@@ -1,3 +1,8 @@
+import user from "./user.js";
+import card from "./card.js";
+import shop from "./shop.js";
+import exchange from "./exchange.js";
+
 export const CUSTOM_ERROR_INFO = {
   40000: "클라이언트 에러입니다",
   40001: "카드 소유량이 부족합니다",
@@ -6,7 +11,9 @@ export const CUSTOM_ERROR_INFO = {
   40009: "알림 정보가 없습니다",
   40010: "상점 정보가 없습니다",
   40011: "포토 카드 정보가 없습니다",
-  40095: "비밀번호는 8자 이상 24자 이하로 입력해주세요",
+  40093: `이메일은 ${user.EMAIL_MIN_LENGTH}자 이상 ${user.EMAIL_MAX_LENGTH}자 이하로 입력해주세요`,
+  40094: `닉네임은 ${user.NICKNAME_MIN_LENGTH}자 이상 ${user.NICKNAME_MAX_LENGTH}자 이하로 입력해주세요`,
+  40095: `비밀번호는 ${user.PASSWORD_MIN_LENGTH}자 이상 ${user.PASSWORD_MAX_LENGTH}자 이하로 입력해주세요`,
   40096: "잘못된 이메일 형식입니다",
   40097: "파일이 없습니다",
   40098: "이메일 또는 비밀번호가 유효하지 않습니다",
