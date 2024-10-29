794e6a76306d71c3d610223c6ff32290558537d3
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-10-29 20:05:18 +0900

    feat : add missing code

--- src/services/mappers/card-mapper.js
+++ src/services/mappers/card-mapper.js
@@ -66,7 +66,7 @@ export function myShopListMapper({ counts, list }) {
   });
   const mappedData = {
     totalCount,
-    countsGroupByGrade: counts,
+    countsGroupByGrade: counts.sortGrade,
     shops: mappedList,
   };
 
