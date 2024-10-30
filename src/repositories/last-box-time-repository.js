b4d615bb5ce048579a72a743f0e8f6cc057d1c53
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-10-30 13:42:19 +0900

    fix : incorrect code

--- src/repositories/last-box-time-repository.js
+++ src/repositories/last-box-time-repository.js
@@ -1,15 +1,15 @@
 import prisma from "./prisma.js";
 
 async function createData({ data, select }) {
-  return await prisma.user.create({ data, select });
+  return await prisma.lastBoxTime.create({ data, select });
 }
 
 async function findFirstData({ where, select }) {
-  return await prisma.user.findFirst({ where, select });
+  return await prisma.lastBoxTime.findFirst({ where, select });
 }
 
 async function updateData({ where, data, select }) {
-  return await prisma.user.update({ where, data, select });
+  return await prisma.lastBoxTime.update({ where, data, select });
 }
 
 export default {
